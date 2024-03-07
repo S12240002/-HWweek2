4e41c9ffeed96a5dab704ce002ddca72c1f938f0
S12240002 <161609468+S12240002@users.noreply.github.com>
2024-03-07 08:51:41 +0800

    Add files via upload

---
+++ 程式語言HW week2 (2).py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  7 08:34:48 2024
+
+@author: Student
+"""
+
+num = int(input('請輸入數字:'))
+if (num % 2) == 0:
+    print(f"{num}是偶數")
+else:
+    print(f"{num}不是偶數")
